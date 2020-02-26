aa5dcf89ef1afb04dadd53b99b0a093dbf81246e
arnoi <mrmagic2003289@gmail.com>
2020-02-26 22:57:03 +0200

    maybe good changes

--- src/main/java/ua/wteam/mbedwars/kits/Kit.java
+++ src/main/java/ua/wteam/mbedwars/kits/Kit.java
@@ -1,46 +1,56 @@
 package ua.wteam.mbedwars.kits;
 
 import org.bukkit.Material;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Kit {
+    
     private List<ItemStack> items;
 
     public Kit(){
-        items = new ArrayList<>();
+        this.items = new ArrayList<>();
+    }
+
+    public Kit(List<ItemStack> items){
+        this.items = items;
     }
 
     public List<ItemStack> getItems() {
         return items;
     }
 
-    public static Builder newBuilder() {
-        return new Kit().new Builder();
+    public Builder newBuilder() {
+        return new Builder();
     }
 
     public class Builder{
 
+        private List<ItemStack> items;
+
+        public Builder(){
+            items = new ArrayList<>();
+        }
+
         public Builder addItems(List<ItemStack> items){
-            Kit.this.items.addAll(items);
+            this.items.addAll(items);
             return this;
         }
 
         public Builder addItem(ItemStack item){
-            Kit.this.items.add(item);
+            this.items.add(item);
             return this;
         }
 
         public Builder addItem(Material item){
-            Kit.this.items.add(new ItemStack(item));
+            this.items.add(new ItemStack(item));
             return this;
         }
 
         public Kit build(){
-            return Kit.this;
+            return new Kit(items);
         }
     }
 }
